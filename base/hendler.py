8b8f5c521d34b261e3a02e738e506e1de14f83e5
mac <smithovich@gmail.com>
2018-09-11 16:53:42 +0300

    refactor project

---
+++ base/hendler.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+import requests
+
+
+class BotHandler(object):
+
+    def __init__(self, token):
+        self.token = token
+        self.api_url = "https://api.telegram.org/bot{}/".format(token)
+
+    def get_updates(self, offset=None, timeout=5):
+        method = 'getUpdates'
+        params = {'timeout': timeout, 'offset': offset}
+        resp = requests.get(self.api_url + method, params)
+        result_json = resp.json()
+        return result_json['result']
+
+    def send_message(self, chat_id, text):
+        params = {'chat_id': chat_id, 'text': text}
+        method = 'sendMessage'
+        resp = requests.post(self.api_url + method, params)
+        return resp
+
+    def get_last_update(self):
+        get_result = self.get_updates()
+
+        if len(get_result) > 0:
+            last_update = get_result[-1]
+        else:
+            last_update = get_result[len(get_result)]
+
+        return last_update
--- main_bot.py
+++ main_bot.py
@@ -1,38 +1,7 @@
-# coding=utf-8
 from setting import setting_bot
-import requests
+from base.hendler import BotHandler
 import datetime
 from rate import currency
-
-
-class BotHandler(object):
-
-    def __init__(self, token):
-        self.token = token
-        self.api_url = "https://api.telegram.org/bot{}/".format(token)
-
-    def get_updates(self, offset=None, timeout=60):
-        method = 'getUpdates'
-        params = {'timeout': timeout, 'offset': offset}
-        resp = requests.get(self.api_url + method, params)
-        result_json = resp.json()
-        return result_json['result']
-
-    def send_message(self, chat_id, text):
-        params = {'chat_id': chat_id, 'text': text}
-        method = 'sendMessage'
-        resp = requests.post(self.api_url + method, params)
-        return resp
-
-    def get_last_update(self):
-        get_result = self.get_updates()
-
-        if len(get_result) > 0:
-            last_update = get_result[-1]
-        else:
-            last_update = get_result[len(get_result)]
-
-        return last_update
 
 
 greet_bot = BotHandler(setting_bot.get('bot_token'))
@@ -48,6 +17,9 @@ def main():
 
     while True:
         print(greet_bot.get_updates(new_offset))
+
+        if not greet_bot.get_updates(new_offset):
+            continue
 
         last_update = greet_bot.get_last_update()
 
